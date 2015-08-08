70b50a5890992465e730009c57475946e5a0272c
yurai <dawid_jurek@vp.pl>
2015-08-08 14:41:12 +0200

    Just replaced preprocessed unordered_map by preprocessed array. 13ms.

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,63 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <ctype.h>
-#include <unordered_map>
-#include <sstream>
 #include <chrono>
-
-std::unordered_map<char, char> complements =
-{
-        {'A',   'T'},
-        {'C',   'G'},
-        {'G',   'C'},
-        {'T',   'A'},
-        {'U',    'A'},
-        {'M',   'K'},
-        {'R',   'Y'},
-        {'W',   'W'},
-        {'S',   'S'},
-        {'Y',   'R'},
-        {'K',   'M'},
-        {'V',   'B'},
-        {'H',   'D'},
-        {'D',   'H'},
-        {'B',   'V'},
-        {'N',   'N'}
-};
+#include <cassert>
+#include <cstring>
 
 const int margin = 60;
+const int range_size = 'z'-'A'+1;
+char upper_complements[range_size];
 
-
-// this is hotspot now
-void upper_complement(std::string &str)
+static inline void upper_complement_v2(std::string &str)
 {
     for (size_t i = 0; i< str.size(); i++)
-        str[i] = complements[toupper(str[i])];
+    {
+        assert('A' <= str[i] && str[i] <= 'z');
+        str[i] = upper_complements[str[i] - 'A'];
+    }
+}
+
+static void init_upper_complements()
+{
+    memset(upper_complements, 0, sizeof(upper_complements));
+    upper_complements['A' - 'A'] =  'T';
+    upper_complements['C' - 'A'] =    'G';
+    upper_complements['G' - 'A'] =    'C';
+    upper_complements['T' - 'A'] =    'A';
+    upper_complements['U' - 'A'] =     'A';
+    upper_complements['M' - 'A'] =    'K';
+    upper_complements['R' - 'A'] =    'Y';
+    upper_complements['W'- 'A'] =    'W';
+    upper_complements['S'- 'A'] =    'S';
+    upper_complements['Y'- 'A'] =    'R';
+    upper_complements['K'- 'A'] =   'M';
+    upper_complements['V' - 'A'] =   'B';
+    upper_complements['H'- 'A'] =    'D';
+    upper_complements['D' - 'A'] =   'H';
+    upper_complements['B' - 'A'] =    'V';
+    upper_complements['N' - 'A'] =    'N';
+
+    upper_complements['a' - 'A'] =  'T';
+    upper_complements['c' - 'A'] =    'G';
+    upper_complements['g' - 'A'] =    'C';
+    upper_complements['t' - 'A'] =    'A';
+    upper_complements['u' - 'A'] =     'A';
+    upper_complements['m' - 'A'] =    'K';
+    upper_complements['r' - 'A'] =    'Y';
+    upper_complements['w'- 'A'] =    'W';
+    upper_complements['s'- 'A'] =    'S';
+    upper_complements['y'- 'A'] =    'R';
+    upper_complements['k'- 'A'] =   'M';
+    upper_complements['v' - 'A'] =   'B';
+    upper_complements['h'- 'A'] =    'D';
+    upper_complements['d' - 'A'] =   'H';
+    upper_complements['b' - 'A'] =    'V';
+    upper_complements['n' - 'A'] =    'N';
 }
 
 // we process accum_sequence from end to begin extracting chunks with size = margin
-void split_and_add_to_buffer(const std::string &accum_sequence, std::string &buffer)
+static void split_and_add_to_buffer(const std::string &accum_sequence, std::string &buffer)
 {
     int current_id = accum_sequence.size();
     while (current_id > 0)
@@ -52,11 +72,14 @@ void split_and_add_to_buffer(const std::string &accum_sequence, std::string &buf
 
 
 /* SUMMARY:
- * now 37ms. With fgets ~35ms, but line has probably extra '\n' on the end.
- * ~60% in _Z16upper_complementRSs + 10% in to_upper
-   So IO is not a problem now.
- * 30.000 allocations (valgind) ~ 3 allocations per loop.
-   ~35.000 cache miss (perf) ~ 4 per loop
+ * Now 13ms
+ * _Z9test_casev (inlined upper_complement_v2) - 20%
+   _int_malloc + __memcpy_sse2_unaligned - 20%
+
+ * Still 30.000 allocations (valgind) ~ 3 allocations per loop.
+   ~30.000 cache miss (perf) ~ 4 per loop
+
+   Symbols view from Qt Creator is buggy - still shows hashtable but they don't exist (objdump helped)
  */
 void test_case()
 {
@@ -65,6 +88,8 @@ void test_case()
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0);
     std::string accum_sequence, buffer, line;
+
+    init_upper_complements();
 
     while (std::getline(std::cin, line))
     {
@@ -79,7 +104,7 @@ void test_case()
         }
         else
         {
-            upper_complement(line);
+            upper_complement_v2(line);
             accum_sequence += line;
         }
     }
