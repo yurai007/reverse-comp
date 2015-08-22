ae87cdb1a32769cdb2f77b8195b06e01b03cb8f8
yurai <dawid_jurek@vp.pl>
2015-08-22 15:39:41 +0200

    Replaced std::string by allocation free c-strings. 7ms.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <chrono>
 #include <cassert>
 #include <cstring>
 
 const int margin = 60;
 const int range_size = 'z'-'A'+1;
-char upper_complements[range_size];
+static char line[margin + 2];
+static char upper_complements[range_size];
 
-static inline void upper_complement_v2(std::string &str)
+static inline void upper_complement_v3(char *str, int str_size)
 {
-    for (size_t i = 0; i< str.size(); i++)
+    for (size_t i = 0; i < str_size; i++)
     {
         assert('A' <= str[i] && str[i] <= 'z');
         str[i] = upper_complements[str[i] - 'A'];
@@ -56,63 +55,100 @@ static void init_upper_complements()
     upper_complements['n' - 'A'] =    'N';
 }
 
-// we process accum_sequence from end to begin extracting chunks with size = margin
-static void split_and_add_to_buffer(const std::string &accum_sequence, std::string &buffer)
+static inline void reverse(char *buffer, int pos, int count)
 {
-    int current_id = accum_sequence.size();
+    int last = pos + count;
+    for (int i = 0; i <= count/2; i++)
+    {
+        char temp = buffer[pos + i];
+        buffer[pos + i] = buffer[last-i];
+        buffer[last-i] = temp;
+    }
+}
+
+static void split_and_add_to_buffer_v2(char *accum_sequence,
+                                       int accum_sequence_size,
+                                       char *buffer,
+                                       int &buffer_size)
+{
+    int current_id = accum_sequence_size;
     while (current_id > 0)
     {
         size_t pos = std::max(int(current_id) - margin, 0);
-        std::string new_line(accum_sequence, pos, current_id - pos);
-        std::reverse(new_line.begin(), new_line.end());
-        buffer += new_line + "\n";
+        reverse(accum_sequence, pos, current_id - pos - 1);
+
+        memcpy(&buffer[buffer_size], &accum_sequence[pos], current_id - pos); //strcpy?
+        buffer_size += current_id - pos;
+        buffer[buffer_size] = '\n';
+        buffer_size++;
+
         current_id -= margin;
     }
 }
 
+/*
+  SUMMARY: Now only 7ms !
 
-/* SUMMARY:
- * Now 13ms
- * _Z9test_casev (inlined upper_complement_v2) - 20%
-   _int_malloc + __memcpy_sse2_unaligned - 20%
+  * Whole program does only 6 allocations (valgrind) (from std::ios_base::sync_with_stdio).
+  So main processing loop is allocation free.
+  Still ~27.000 cache misses (~ 3.5 per loop)
+  Lots branches (320/loop/line). But only 3 branch misses per loop.
+  75% time is spent in upper_complement_v3
 
- * Still 30.000 allocations (valgind) ~ 3 allocations per loop.
-   ~30.000 cache miss (perf) ~ 4 per loop
 
-   Symbols view from Qt Creator is buggy - still shows hashtable but they don't exist (objdump helped)
+  hey, what about custom fast_string class?
  */
-void test_case()
+void test_case_allocation_free()
 {
     auto t1 = std::chrono::high_resolution_clock::now();
 
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0);
-    std::string accum_sequence, buffer, line;
+
+    static char accum_sequence[16*1024];
+    int accum_seq_size = 0;
+
+    static char buffer[1024*1024];
+    int buffer_size = 0;
+
+    int line_size = 0;
+    memset(line, 0, sizeof(line));
 
     init_upper_complements();
 
-    while (std::getline(std::cin, line))
+    while (fgets_unlocked(line, margin+2, stdin))
     {
+        // line contains sequence \n,0 but we ommit that immediatly after reading
+        line_size = strlen(line);
+        line_size--;
         if (line[0] == '>')
         {
-            if (!accum_sequence.empty())
+            if (accum_seq_size != 0)
             {
-                split_and_add_to_buffer(accum_sequence, buffer);
-                accum_sequence.clear();
+                split_and_add_to_buffer_v2(accum_sequence, accum_seq_size,
+                                           buffer, buffer_size);
+                accum_seq_size = 0;
             }
-            buffer += line + '\n'; // extra allocation in buffer + memcpy from line
+
+            memcpy(&buffer[buffer_size], line, line_size);
+            buffer_size += line_size;
+            buffer[buffer_size] = '\n';
+            buffer_size++;
         }
         else
         {
-            upper_complement_v2(line);
-            accum_sequence += line;
+            upper_complement_v3(line, line_size);
+
+            memcpy(&accum_sequence[accum_seq_size], line, line_size);
+            accum_seq_size += line_size;
         }
     }
 
-    if (!accum_sequence.empty())
+    if (accum_seq_size != 0)
     {
-        split_and_add_to_buffer(accum_sequence, buffer);
-        accum_sequence.clear();
+        split_and_add_to_buffer_v2(accum_sequence, accum_seq_size,
+                                   buffer, buffer_size);
+        accum_seq_size = 0;
     }
 
     std::cout << buffer;
@@ -122,8 +158,52 @@ void test_case()
     std::cerr << "time: " << s.count() << " ms \n";
 }
 
+static void unit_tests()
+{
+    {
+        char buffer[] = "0123456789";
+        reverse(buffer, 0, 0);
+    }
+    {
+        char buffer[] = "0123456789";
+        reverse(buffer, 0, 1);
+    }
+    {
+        char buffer[] = "0123456789";
+        reverse(buffer, 0, 2);
+    }
+    {
+        char buffer[] = "0123456789";
+        reverse(buffer, 0, 3);
+    }
+    {
+        char buffer[] = "0123456789";
+        reverse(buffer, 0, 9);
+    }
+    {
+        char buffer[] = "0123456789";
+        reverse(buffer, 0, 9);
+    }
+
+    char accum_sequence[] = "123CGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGGGAGGCCGAGGCGGGC456"
+    "012CCTGAGGTCAGGAGTTCGAGACCAGCCTGGCCAACATGGTGAAACCCCGTCTCT789"
+    "AAAAATACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTGTAATCCCAGCTACTCGGGAG"
+    "GCTGAGGCAGGAGAATCGCTT";
+    int accum_sequence_size = strlen(accum_sequence);
+
+    char buffer[512];
+    int buffer_size = 0;
+
+
+    split_and_add_to_buffer_v2(accum_sequence, accum_sequence_size, buffer,
+                               buffer_size);
+    printf(":)\n");
+
+}
+
+
 int main()
 {
-    test_case();
+    test_case_allocation_free();
     return 0;
 }
